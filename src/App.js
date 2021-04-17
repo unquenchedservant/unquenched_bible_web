738ffce153e36797cba89e9f2f1acc2523801b45
Jonathan Thorne <jonathanthorne13@gmail.com>
2021-04-17 14:14:52 -0400

    Additional try at drawer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,4 @@
 import logo from './logo.svg';
-import './App.css';
-import React from 'react';
-import { Button } from '@material-ui/core';
 
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
