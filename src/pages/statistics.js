b11af588b47d9a2479816583b2caebe8d7f05861
Jonathan Thorne <jonathanthorne13@gmail.com>
2021-04-19 10:03:40 -0400

    begin adding statistics page with firebase data

--- src/pages/statistics.js
+++ src/pages/statistics.js
@@ -1,9 +1,58 @@
 import React from 'react';
+import firebase from "firebase/app";
+import "firebase/firestore";
 
-function Statistics(){
-	return(
-		<div>Statistics</div>
-	);
+class Statistics extends React.Component{
+	
+	constructor(props)
+	{
+	    super(props);
+	    this.state = {
+	        data: null,
+	    };
+	}
+
+	
+	componentDidMount(){
+		var db = firebase.firestore();
+		const user = firebase.auth().currentUser;
+		const docRef = db.collection('main').doc(user.uid);
+		docRef.get().then((doc) => {
+			if (doc.exists) {
+				let data = doc.data();
+				this.setState({ 
+					data: data 
+				});
+				console.log("Document data:", data);
+			}else{
+				this.setState({ data: null });
+				console.log("No such document");
+			}
+		}).catch(function (error) {
+			this.setState({ data:null });
+			console.log("Error getting document:", error);
+		});
+	}
+	
+	render() {
+		let dataUI = this.state.data === null ? null : <pre>{JSON.stringify(this.state.data)}</pre>;
+		let currentStreak = "Loading...";
+		let maxStreak = "Loading...";
+		if(dataUI){
+			currentStreak = this.state.data["currentStreak"];
+			maxStreak = this.state.data["maxStreak"];
+		}
+
+		return(
+			<div>
+				<ul>
+					<li>Current Streak - {currentStreak}</li>
+					<li>Max Streak - {maxStreak}</li>
+				</ul>
+			</div>
+		);
+
+	}
 }
 
 export default Statistics;
